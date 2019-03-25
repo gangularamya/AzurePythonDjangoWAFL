904254c8935076a4ddb9fd5189e2b290c4be40af
Ramya Gangula <ramya1331@gmail.com>
2019-03-25 16:19:26 -0500

    added postgresql connection strings in settings.py file

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASENAME', ''),
+        'USER': os.environ.get('DATABASEUSER', ''),
+        'PASSWORD':  os.environ.get('DATABASEPASSWORD', ''),
+        'HOST': os.environ.get('DATABASEHOST', ''),
+        'PORT': '5432',
     }
 }
 
