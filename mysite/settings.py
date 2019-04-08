1278c43a9b0600422937188d54f6172175b8b8c5
Ramya Gangula <ramya1331@gmail.com>
2019-04-08 14:52:57 -0500

    reverting back postgresql changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASENAME', ''),
-        'USER': os.environ.get('DATABASEUSER', ''),
-        'PASSWORD':  os.environ.get('DATABASEPASSWORD', ''),
-        'HOST': os.environ.get('DATABASEHOST', ''),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
