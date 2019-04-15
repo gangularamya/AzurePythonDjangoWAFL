c515a56f28e1e0c880c661d1b1e6b4d04766807b
Ramya Gangula <ramya1331@gmail.com>
2019-04-15 14:43:03 -0500

    updated urls.py to accept / route

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('coffeeshop/', include('coffeeshop.urls')),
+    path('', include('coffeeshop.urls')),
 ]
